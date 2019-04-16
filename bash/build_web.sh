23a04e99a96dfb4e6abc04da1a6a663c044baf4e
panxiqun <panxiqun@pinquest.cn>
2019-04-16 19:30:22 +0800

    test

--- bash/build_web.sh
+++ bash/build_web.sh
@@ -9,21 +9,23 @@ prodServer="root@120.79.155.84"
 DeployPath="/home/pinfire/weblogic/public/$2"
 
 cd $projectDir
-npm i --registry=https://registry.npm.taobao.org
+
+npm config set strict-ssl false 
+npm i -s -f --no-audit --no-package-lock --package-lock-only --global-style --no-shrinkwrap --reg=https://registry.npm.taobao.org
 
 case "$5" in
   "refs/heads/master")
     npm run build
     ssh $prodServer mkdir -p $DeployPath
     rsync -avz --progress dist/* $prodServer:$DeployPath
-    echo "deploy prod server successfully"
+    echo "deploy $2 prod server successfully"
     ;;
 
   "refs/heads/develop")
     npm run build:dev
     mkdir -p $DeployPath
     cp -ufr ./dist/* $DeployPath
-    echo "deploy develop server successfully"
+    echo "deploy $2 develop server successfully"
     ;;
 esac
 
